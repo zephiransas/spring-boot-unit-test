5db5eac918eb989182f2f78f2045c2fa6d106152
Takafumi Yoshida <zephiransas@gmail.com>
2017-03-02 17:29:25 +0900

    Run test in transaction

--- src/test/java/com/example/EmployeeRepositoryTest.java
+++ src/test/java/com/example/EmployeeRepositoryTest.java
@@ -6,26 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.transaction.Transactional;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
-public class EmployeeRepositoryTest extends ApplicationTest {
+@Transactional
+public class EmployeeRepositoryTest {
 
     @Autowired
     EmployeeRepository repository;
 
     @Test
     public void test() throws Exception {
-
         Employee employee = new Employee();
         employee.setName("test");
         employee.setEmail("test@example.com");
         repository.save(employee);
 
         assertThat(repository.findAll().size(), is(1));
-
     }
 
     @Test
